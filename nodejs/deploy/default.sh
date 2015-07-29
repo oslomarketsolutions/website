119fd735539f6493078581c460215ab2a47bc749
Andreas Holen <andreas.holen@oslomarketsolutions.no>
2015-07-29 13:22:38 +0200

    DRIF-271 www-demo

---
+++ nodejs/deploy/default.sh
@@ -0,0 +1,14 @@
+username="www"
+
+target_prefix="www"
+
+input_relative="."
+
+output_dir="nodejss"
+output_append_svnversion=1
+
+deployer="deploy_nodejs.sh"
+replacer="replacer.sh"
+
+activate_script="activate_nodejs.sh"
+startstop_script="startstop_nodejs.sh"
