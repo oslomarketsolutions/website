8e71d09f54e86c0e81f3689fd626ff068152decd
Ørjan Knutli <orjan.knutli@oslomarketsolutions.no>
2015-10-01 15:25:21 +0200

    small changes for debug

--- client/routes/views/portfolios.js
+++ client/routes/views/portfolios.js
@@ -1,10 +1,10 @@
 var keystone = require('keystone');
 var async = require('async');
 exports = module.exports = function(req, res) {
-  
+
   var view = new keystone.View(req, res);
   var locals = res.locals;
-  
+
   // Init locals
   locals.section = 'news';
   locals.filters = {
@@ -14,34 +14,34 @@ exports = module.exports = function(req, res) {
     posts: [],
     categories: []
   };
-  
+
   // Load all categories
   view.on('init', function(next) {
-    
+
     keystone.list('PostCategory').model.find().sort('name').exec(function(err, results) {
-      
+
       if (err || !results.length) {
         return next(err);
       }
-      
+
       locals.data.categories = results;
-      
+
       // Load the counts for each category
       async.each(locals.data.categories, function(category, next) {
-        
+
         keystone.list('Post').model.count().where('categories').in([category.id]).exec(function(err, count) {
           category.postCount = count;
           next(err);
         });
-        
+
       }, function(err) {
         next(err);
       });
-      
+
     });
-    
+
   });
-  
+
   // Load the current category filter
   view.on('init', function(next) {
       keystone.list('PostCategory').model.findOne({ key: locals.filters.category }).exec(function(err, result) {
@@ -51,9 +51,9 @@ exports = module.exports = function(req, res) {
     // } else {
     //   next();
     // }
-    
+
   });
-  
+
   // Load the posts
   view.on('init', function(next) {
     var q = keystone.list('Post').paginate({
@@ -64,7 +64,7 @@ exports = module.exports = function(req, res) {
       .where('state', 'published')
       .sort('-publishedDate')
       .populate('author categories');
-      
+
     q.exec(function(err, results) {
 
         var isNorwegian = req.url.substr(0,4) !== '/eng'
@@ -72,11 +72,11 @@ exports = module.exports = function(req, res) {
           var hasEnglish = false;
           var hasPortfolio = false;
           item.categories.forEach(function (category) {
-            
+
             if (category.key === 'english') {
               hasEnglish = true;
-            } 
-            if (category.key === 'portfolio') {
+            }
+            if (category.key === 'projects') {
               hasPortfolio = true;
             }
           });
@@ -84,12 +84,12 @@ exports = module.exports = function(req, res) {
         });
       next(err);
     });
-    
+
   });
 
 
-  
+
   // Render the view
   view.render('portfolios');
-  
+
 };
