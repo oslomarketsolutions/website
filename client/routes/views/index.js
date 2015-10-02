2996b24e143a765f391eb6429104681054fdb5bf
Ørjan Knutli <orjan.knutli@oslomarketsolutions.no>
2015-10-02 11:19:18 +0200

    fixed more pages from portfolio to projects

--- client/routes/views/index.js
+++ client/routes/views/index.js
@@ -13,38 +13,38 @@ exports = module.exports = function(req, res) {
     posts: [],
     categories: []
   };
-  
+
   // Load all categories
   view.on('init', function(next) {
-    
+
     keystone.list('PostCategory').model.find().sort('name').exec(function(err, results) {
-      
+
 
       if (err || !results.length) {
         return next(err);
       }
       // locals.data.categories = [results];
       locals.data.categories = [{ _id: '55a3bc70a042fc5e1128bc74', key: 'blog', name: 'Blog', __v: 0 }]
-      
+
       // Load the counts for each category
       async.each(locals.data.categories, function(category, next) {
-        
+
         keystone.list('Post').model.count().where('categories').in([category.id]).exec(function(err, count) {
           category.postCount = count;
           next(err);
         });
-        
+
       }, function(err) {
         next(err);
       });
-      
+
     });
-    
+
   });
-  
+
   // Load the current category filter
   view.on('init', function(next) {
-    
+
     if (req.params.category) {
       keystone.list('PostCategory').model.findOne({ key: "blog"}).exec(function(err, result) {
         locals.data.category = result;
@@ -53,9 +53,9 @@ exports = module.exports = function(req, res) {
     } else {
       next();
     }
-    
+
   });
-  
+
   // Load the posts
   view.on('init', function(next) {
     var q = keystone.list('Post').paginate({
@@ -66,11 +66,11 @@ exports = module.exports = function(req, res) {
       .where('state', 'published')
       .sort('-publishedDate')
       .populate('author categories');
-    
+
     if (locals.data.category) {
       q.where('categories').in([locals.data.category]);
     }
-    
+
     q.exec(function(err, results) {
 
         var isNorwegian = req.url.substr(0,4) !== '/eng'
@@ -78,11 +78,11 @@ exports = module.exports = function(req, res) {
           var hasEnglish = false;
           var hasPortfolio = false;
           item.categories.forEach(function (category) {
-            
+
             if (category.key === 'english') {
               hasEnglish = true;
-            } 
-            if (category.key === 'portfolio') {
+            }
+            if (category.key === 'projects') {
               hasPortfolio = true;
             }
           });
@@ -90,14 +90,14 @@ exports = module.exports = function(req, res) {
         });
       next(err);
     });
-    
+
   });
 
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
-	
+
 	// Render the view
 	view.render('index');
-	
+
 };
