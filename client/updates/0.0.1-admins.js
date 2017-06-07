dd2602aad92893c36b8794bbc5b525ba0e361f10
Haakon Winther <haakon@oslomarketsolutions.no>
2017-06-07 17:15:36 +0200

    OMS-18 New branch for new logo

--- client/updates/0.0.1-admins.js
+++ client/updates/0.0.1-admins.js
@@ -2,16 +2,16 @@
  * This script automatically creates a default Admin user when an
  * empty database is used for the first time. You can use this
  * technique to insert data into any List you have defined.
- * 
+ *
  * Alternatively, you can export a custom function for the update:
  * module.exports = function(done) { ... }
  */
 
-//exports.create = {
-	//User: [
-		//{ 'name.first': 'Admin', 'name.last': 'User', email: 'orjan@manamind.com', password: 'test', isAdmin: true }
-	//]
-//};
+exports.create = {
+	User: [
+		{ 'name.first': 'Admin', 'name.last': 'User', email: 'drift@oslomarketsolutions.no', password: 'sQEK,k7BLqM)Exk$!', isAdmin: true }
+	]
+};
 
 /*
 
@@ -26,9 +26,9 @@ var admins = [
 ];
 
 function createAdmin(admin, done) {
-	
+
 	var newAdmin = new User.model(admin);
-	
+
 	newAdmin.isAdmin = true;
 	newAdmin.save(function(err) {
 		if (err) {
@@ -39,7 +39,7 @@ function createAdmin(admin, done) {
 		}
 		done(err);
 	});
-	
+
 }
 
 exports = module.exports = function(done) {
