57cb20efc2aa0a0feb9480aa2a6da5a94de55853
Åsmund Staldvik <aasmund.staldvik@gmail.com>
2018-06-14 11:03:13 +0200

    Fixed some styling on employeeCard

--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -1,30 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import logo from '../img/github-icon.svg';
+import './EmployeeCard.css';
 
 const EmployeeCard = props => {
-  const { name, position, description } = props;
-
-  const cardStyle = {
-    border: '1px solid #eaecee',
-    padding: '2em 4em',
-    width: '80%',
-    textAlign: 'center',
-    boxShadow: '3px 3px 4px',
-  };
-
-  const imageStyle = {
-    width: 'auto',
-    height: '200px',
-  };
+  const { name, position, description, image } = props;
 
   return (
-    <div className="content" style={cardStyle}>
-      <img src={logo} alt="Just the Github-logo" style={imageStyle} />
-      <h1>{name}</h1>
-      <h2>{position}</h2>
-      <p>{description}</p>
+    <div className="employeeCard">
+      <div>
+        <img src={image} alt="Stockphoto of a girl smiling" />
+      </div>
+
+      <div className="employeeTextContainer" style={{ flex: 1 }}>
+        <div className="employeeHeader">
+          <h2 id="employeeName">{name}</h2>
+          <h3 id="employeePosition">{position}</h3>
+          <p id="employeeIcon">ICON</p>
+        </div>
+
+        <div className="employeeFooter">
+          <p id="employeeDescription">{description}</p>
+        </div>
+      </div>
     </div>
   );
 };
@@ -33,6 +30,7 @@ EmployeeCard.propTypes = {
   name: PropTypes.string,
   position: PropTypes.string,
   description: PropTypes.string,
+  image: PropTypes.string,
 };
 
 export default EmployeeCard;
