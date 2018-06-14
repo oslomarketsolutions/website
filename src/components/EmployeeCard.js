13de7a6c6a7ffab231e6e92de0145211f194e023
Åsmund Staldvik <aasmund.staldvik@gmail.com>
2018-06-14 14:05:11 +0200

    Styled some more and added a couple of images

--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import fontawesome from '@fortawesome/fontawesome';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faCoffee from '@fortawesome/fontawesome-free-solid/faCoffee';
 import './EmployeeCard.css';
+
+// only temp
+import pic from '../../static/img/janne.jpg';
 
 const EmployeeCard = props => {
   const { name, position, description, image } = props;
@@ -8,14 +14,14 @@ const EmployeeCard = props => {
   return (
     <div className="employeeCard">
       <div>
-        <img src={image} alt="Stockphoto of a girl smiling" />
+        <img src={pic} alt="Stockphoto of a girl smiling" />
       </div>
 
       <div className="employeeTextContainer" style={{ flex: 1 }}>
         <div className="employeeHeader">
           <h2 id="employeeName">{name}</h2>
           <h3 id="employeePosition">{position}</h3>
-          <p id="employeeIcon">ICON</p>
+          <FontAwesomeIcon icon={faCoffee} id="employeeIcon" />
         </div>
 
         <div className="employeeFooter">
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-
+import EmployeeCard from '../components/EmployeeCard';
 import Navbar from '../components/Navbar';
 import './style.css';
 
@@ -13,9 +13,20 @@ const TemplateWrapper = ({ children, location }) => {
     <React.Fragment>
       <Helmet title="Oslo Market Solutions">
         <html lang={lang} />
+        <link
+          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-
+        awesome.min.css"
+          rel="stylesheet"
+          integrity="sha384-
+        wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+          crossOrigin="anonymous"
+        />
       </Helmet>
-      <Navbar lang={lang} />
-      {children()}
+      <EmployeeCard
+        name="Janne Johnsen"
+        position="Front-end Developer"
+        description="Liker et eller annet"
+      />
     </React.Fragment>
   );
 };
