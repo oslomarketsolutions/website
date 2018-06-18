d79ea22ef6da90eb860a7d7eed96322c0da9d453
Elias Thøgersen <elias3004@msn.com>
2018-06-18 17:29:31 +0200

    OMS-28/ Oppsett navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,44 +3,51 @@ import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 
 import github from '../img/github-icon.svg';
-import logo from '../img/logo.svg';
+import logo from '../img/logo_oms_hoved.png';
+
+function changeLinkLang(lang) {
+  const links = { about: 'About', work: 'Work', products: 'Products' };
+
+  if (lang === 'no') {
+    links.about = 'Om oss';
+    links.work = 'Jobb';
+    links.products = 'Produkter';
+  }
+
+  return links;
+}
 
 const Navbar = ({ lang }) => {
   const base = `/${lang}`;
+  const link = changeLinkLang(lang);
 
   return (
-    <nav className="navbar is-transparent">
-      <div className="container">
-        <div className="navbar-brand">
-          <Link to={base} className="navbar-item">
-            <figure className="image">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-            </figure>
-          </Link>
-        </div>
-        <div className="navbar-start">
-          <Link className="navbar-item" to={`${base}/about`}>
-            About
-          </Link>
-          <Link className="navbar-item" to={`${base}/products`}>
-            Products
-          </Link>
-          <Link className="navbar-item" to={`${base}/career`}>
-            Career
-          </Link>
-        </div>
-        <div className="navbar-end">
-          <a
-            className="navbar-item"
-            href="https://github.com/oslomarketsolutions/website"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span className="icon">
-              <img src={github} alt="Github" />
-            </span>
-          </a>
-        </div>
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to={base} className="navbar-item">
+          <img src={logo} alt="Oms" />
+        </Link>
+      </div>
+      <div className="navbar-links">
+        <Link className="navbar-item" to={`${base}/products`}>
+          {link.products}
+        </Link>
+        <Link className="navbar-item" to={`${base}/career`}>
+          {link.work}
+        </Link>
+        <Link className="navbar-item" to={`${base}/about`}>
+          {link.about}
+        </Link>
+        <a
+          className="navbar-item last"
+          href="https://github.com/oslomarketsolutions/website"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="icon">
+            <img src={github} alt="Github" />
+          </span>
+        </a>
       </div>
     </nav>
   );
