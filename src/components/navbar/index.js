e8df30cd8ae42ba2af30f749e735dccf78eeaafe
Elias Thøgersen <elias3004@msn.com>
2018-06-21 10:37:28 +0200

    Fixed function syntax and cleaned up nav markup

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,57 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
-import github from '../../img/github-icon.svg';
 import logo from '../../img/logo_oms_hoved.png';
 import styles from './navbar.module.scss';
 
-function changeLinkLang(lang) {
+const changeLinkLanguage = language => {
   const links = { about: 'About', work: 'Work', products: 'Products' };
 
-  if (lang === 'no') {
+  if (language === 'no') {
     links.about = 'Om oss';
     links.work = 'Jobb';
     links.products = 'Produkter';
   }
 
   return links;
-}
+};
 
-const Navbar = ({ lang }) => {
-  const base = `/${lang}`;
-  const link = changeLinkLang(lang);
+const Navbar = ({ language }) => {
+  const base = `/${language}`;
+  const link = changeLinkLanguage(language);
 
   return (
     <nav className={styles.navbar}>
-      <div className={styles.navbarBrand}>
-        <Link to={base}>
-          <img src={logo} alt="Oms" />
-        </Link>
-      </div>
-      <div className={styles.navbarLinks}>
-        <Link to={`${base}/products`}>{link.products}</Link>
-        <Link to={`${base}/career`}>{link.work}</Link>
-        <Link to={`${base}/about`}>{link.about}</Link>
-        <a
-          href="https://github.com/oslomarketsolutions/website"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span>
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
+      <ul>
+        <li>
+          <Link to={base}>
+            <img src={logo} alt="Oms logo" />
+          </Link>
+        </li>
+        <li>
+          <Link to={`${base}/products`}>{link.products}</Link>
+        </li>
+        <li>
+          <Link to={`${base}/career`}>{link.work}</Link>
+        </li>
+        <li>
+          <Link to={`${base}/about`}>{link.about}</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
 
 Navbar.propTypes = {
-  lang: PropTypes.string,
+  language: PropTypes.string,
 };
 
 Navbar.defaultProps = {
-  lang: 'no',
+  language: 'no',
 };
 
 export default Navbar;
