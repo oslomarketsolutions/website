c74d231ff185e9e336072917e5d905c945c1858e
Åsmund Staldvik <aasmund.staldvik@gmail.com>
2018-06-21 12:53:43 +0200

    Still trying to fix eslint-error

--- src/components/featureCard/index.js
+++ src/components/featureCard/index.js
@@ -1,39 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { navigateTo } from 'gatsby-link';
+import Link from 'gatsby-link';
 import styles from './FeatureCard.module.scss';
 
 const FeatureCard = props => {
   // link should be the slug for whatever the featurecard is linking to
-  const { title, description, image, features, link } = props;
+  const { title, description, image, features, link = 'test' } = props;
   return (
-    <article
-      role="link"
-      tabIndex="0"
-      className={styles.FeatureCard}
-      onClick={() => navigateTo(link)}
-      onKeyDown={event => {
-        if (event.key === 'Enter') navigateTo(link);
-      }}
-    >
-      <section className={styles.image}>
-        <img src={image} alt={title} />
-      </section>
-      <section className={styles.header}>
-        <h3>{title}</h3>
-      </section>
-      <section className={styles.description}>
-        <p>{description}</p>
-      </section>
-      <section className={styles.footer}>
-        <ul>
-          {features &&
-            features.map(feature => {
-              return <li key={feature}>{feature}</li>;
-            })}
-        </ul>
-      </section>
-    </article>
+    <Link to={link} className={styles.FeatureCard}>
+      <article>
+        <section className={styles.image}>
+          <img src={image} alt={title} />
+        </section>
+        <section className={styles.header}>
+          <h3>{title}</h3>
+        </section>
+        <section className={styles.description}>
+          <p>{description}</p>
+        </section>
+        <section className={styles.footer}>
+          <ul>
+            {features &&
+              features.map(feature => {
+                return <li key={feature}>{feature}</li>;
+              })}
+          </ul>
+        </section>
+      </article>
+    </Link>
   );
 };
 
