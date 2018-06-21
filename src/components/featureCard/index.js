a489277c803e7a26d98a0e44668be69568504e83
Åsmund Staldvik <aasmund.staldvik@gmail.com>
2018-06-21 13:02:30 +0200

    removed some html

--- src/components/featureCard/index.js
+++ src/components/featureCard/index.js
@@ -5,26 +5,24 @@ import styles from './FeatureCard.module.scss';
 
 const FeatureCard = props => {
   // link should be the slug for whatever the featurecard is linking to
-  const { title, description, image, features, link = 'test' } = props;
+  const { title, description, image, features, link = '' } = props;
   return (
     <Link to={link} className={styles.FeatureCard}>
-      <article>
-        <section className={styles.image}>
-          <img src={image} alt={title} />
-        </section>
-        <section className={styles.header}>
-          <h3>{title}</h3>
-        </section>
-        <section className={styles.description}>
-          <p>{description}</p>
-        </section>
-        <section className={styles.footer}>
-          <ul>
-            {features &&
-              features.map(feature => <li key={feature}>{feature}</li>)}
-          </ul>
-        </section>
-      </article>
+      <section className={styles.image}>
+        <img src={image} alt={title} />
+      </section>
+      <section className={styles.header}>
+        <h3>{title}</h3>
+      </section>
+      <section className={styles.description}>
+        <p>{description}</p>
+      </section>
+      <section className={styles.footer}>
+        <ul>
+          {features &&
+            features.map(feature => <li key={feature}>{feature}</li>)}
+        </ul>
+      </section>
     </Link>
   );
 };
