4d3a77c47c800ee95c2229c5b950675d4f35807e
Åsmund Staldvik <aasmund.staldvik@gmail.com>
2018-06-25 13:53:49 +0200

    Edited away background

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import favicon from '../img/favicon_oms.jpg';
+import favicon from '../img/favicon_oms.png';
 
 import './style.scss';
 import Navbar from '../components/navbar/index';
@@ -13,7 +13,7 @@ const TemplateWrapper = ({ children, location }) => {
   return (
     <React.Fragment>
       <Helmet title="Oslo Market Solutions">
-        <link rel="icon" type="image/jpg" href={favicon} />
+        <link rel="icon" type="image/png" href={favicon} />
         <html lang={language} />
       </Helmet>
       <Navbar language={language} />
