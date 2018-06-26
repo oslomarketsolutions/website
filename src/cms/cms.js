9f5b438d7b11ffac3b242de9e2450348a53fc749
Åsmund Staldvik <aasmund.staldvik@gmail.com>
2018-06-26 11:26:57 +0200

    CMS now shows EmployeeCard while editing

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,10 +4,10 @@ import AboutPagePreview from './preview-templates/AboutPagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
 import ProductPagePreview from './preview-templates/ProductPagePreview';
 import EmployeePreview from './preview-templates/EmployeePreview';
-import styles from '../components/employeesCard/EmployeeCard.module.scss';
 
 CMS.registerPreviewStyle('/styles.css');
 CMS.registerPreviewTemplate('about', AboutPagePreview);
 CMS.registerPreviewTemplate('norwegian_employees', EmployeePreview);
+CMS.registerPreviewTemplate('english_employees', EmployeePreview);
 CMS.registerPreviewTemplate('products', ProductPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
--- src/cms/preview-templates/EmployeePreview.js
+++ src/cms/preview-templates/EmployeePreview.js
@@ -3,7 +3,15 @@ import PropTypes from 'prop-types';
 import EmployeeCard from '../../components/employeesCard';
 
 const EmployeePreview = ({ entry, widgetFor }) => {
-  return <EmployeeCard name={entry.getIn(['data', 'title'])} />;
+  return (
+    <EmployeeCard
+      name={entry.getIn(['data', 'title'])}
+      jobTitle={entry.getIn(['data', 'jobTitle'])}
+      description={entry.getIn(['data', 'description'])}
+      image={entry.getIn(['data', 'image'])}
+      jobType={entry.getIn(['data', 'jobType'])}
+    />
+  );
 };
 
 EmployeePreview.propTypes = {
