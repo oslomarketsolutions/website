71705e61f14f9b1c55c49b5978262b69957dad36
Åsmund Staldvik <aasmund.staldvik@gmail.com>
2018-06-27 15:33:47 +0200

    Fixed ESLint-errors

--- src/cms/preview-templates/EmployeePreview.js
+++ src/cms/preview-templates/EmployeePreview.js
@@ -2,23 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import EmployeeCard from '../../components/employeesCard';
 
-const EmployeePreview = ({ entry, widgetFor }) => {
-  return (
-    <EmployeeCard
-      name={entry.getIn(['data', 'title'])}
-      jobTitle={entry.getIn(['data', 'jobTitle'])}
-      description={entry.getIn(['data', 'description'])}
-      image={entry.getIn(['data', 'image'])}
-      jobType={entry.getIn(['data', 'jobType'])}
-    />
-  );
-};
+const EmployeePreview = ({ entry }) => (
+  <EmployeeCard
+    name={entry.getIn(['data', 'title'])}
+    jobTitle={entry.getIn(['data', 'jobTitle'])}
+    description={entry.getIn(['data', 'description'])}
+    image={entry.getIn(['data', 'image'])}
+    jobType={entry.getIn(['data', 'jobType'])}
+  />
+);
 
 EmployeePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
-  widgetFor: PropTypes.func,
 };
 
 export default EmployeePreview;
