31f578b491a54c01914b94f139c6d0a0dd4867ad
Elias Thøgersen <elias3004@msn.com>
2018-06-27 16:34:59 +0200

    Styled navbar and added language change functionality
    Styled navbar according to sketches and added functionality to change
    between the norwegian and english pages.
    Also added fontawesome library to get easier access to icons.

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import Link from 'gatsby-link';
 import logo from '../../img/logo_oms_hoved.png';
 import styles from './navbar.module.scss';
 
 const changeLinkLanguage = language => {
-  const links = { about: 'About us', work: 'Work', products: 'Products' };
+  const links = { about: 'ABOUT US', work: 'WORK', products: 'PRODUCTS' };
 
   if (language === 'no') {
-    links.about = 'Om oss';
-    links.work = 'Jobb';
-    links.products = 'Produkter';
+    links.about = 'OM OSS';
+    links.work = 'JOBB';
+    links.products = 'PRODUKTER';
   }
 
   return links;
 };
 
-const Navbar = ({ language }) => {
+const changePageLanguage = (path, language) => {
+  const pathSplitArray = path.split('/');
+  let returnPath;
+
+  if (language === 'en')
+    if (pathSplitArray.length < 3) returnPath = 'no';
+    else returnPath = `/no/${pathSplitArray[2]}`;
+  else if (pathSplitArray.length < 3) returnPath = 'en';
+  else returnPath = `/en/${pathSplitArray[2]}`;
+
+  return returnPath;
+};
+
+const Navbar = ({ language, location }) => {
   const base = `/${language}`;
   const link = changeLinkLanguage(language);
 
@@ -29,13 +45,66 @@ const Navbar = ({ language }) => {
           </Link>
         </li>
         <li>
-          <Link to={`${base}/products`}>{link.products}</Link>
+          <Link activeClassName={styles.active} to={`${base}/products`}>
+            {link.products}
+          </Link>
         </li>
         <li>
-          <Link to={`${base}/career`}>{link.work}</Link>
+          <Link activeClassName={styles.active} to={`${base}/career`}>
+            {link.work}
+          </Link>
         </li>
-        <li>
-          <Link to={`${base}/about`}>{link.about}</Link>
+        <li className={styles.doublePadding}>
+          <Link activeClassName={styles.active} to={`${base}/about`}>
+            {link.about}
+          </Link>
+        </li>
+        <li className={`${styles.noPaddingRight} ${styles.borderLeft}`}>
+          <a
+            href="https://www.linkedin.com/company/oslo-market-solutions-as/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.noHover}
+          >
+            <FontAwesomeIcon icon={['fab', 'linkedin']} />
+          </a>
+        </li>
+        <li className={styles.noPaddingRight}>
+          <a
+            href="https://www.facebook.com/oslomarketsolutions/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.noHover}
+          >
+            <FontAwesomeIcon icon={['fab', 'facebook']} />
+          </a>
+        </li>
+        <li className={styles.noPaddingRight}>
+          <a
+            href="https://medium.com/shark-bytes"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.noHover}
+          >
+            <FontAwesomeIcon icon={['fab', 'medium']} />
+          </a>
+        </li>
+        <li className={styles.borderRight}>
+          <a
+            href="https://github.com/oslomarketsolutions/website"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.noHover}
+          >
+            <FontAwesomeIcon icon={['fab', 'github-square']} />
+          </a>
+        </li>
+        <li className={styles.noPaddingRight}>
+          <Link to={changePageLanguage(location.pathname, language)}>
+            {/* <img src={language === 'no' ? no : en} alt="flag" /> */}
+            <FontAwesomeIcon icon="globe" />
+            {language === 'no' ? 'ENGLISH' : 'NORSK'}
+          </Link>
         </li>
       </ul>
     </nav>
@@ -44,6 +113,7 @@ const Navbar = ({ language }) => {
 
 Navbar.propTypes = {
   language: PropTypes.string,
+  location: PropTypes.string,
 };
 
 Navbar.defaultProps = {
