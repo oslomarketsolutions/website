b85a9c07bc9ed61bb5e03f370b83860dd20bb04d
Åsmund Staldvik <aasmund.staldvik@gmail.com>
2018-06-28 14:30:49 +0200

    Tried removing index files and registerPreview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,15 +1,15 @@
 import CMS from 'netlify-cms';
 
 import AboutPagePreview from './preview-templates/AboutPagePreview';
-import IndexPagePreview from './preview-templates/IndexPagePreview';
+//import IndexPagePreview from './preview-templates/IndexPagePreview';
 import EmployeePreview from './preview-templates/EmployeePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
 import ProductPagePreview from './preview-templates/ProductPagePreview';
 
 CMS.registerPreviewStyle('/styles.css');
 CMS.registerPreviewTemplate('about', AboutPagePreview);
-CMS.registerPreviewTemplate('norwegian_index_page', IndexPagePreview);
-CMS.registerPreviewTemplate('english_index_page', IndexPagePreview);
+//CMS.registerPreviewTemplate('norwegian_index_page', IndexPagePreview);
+//CMS.registerPreviewTemplate('english_index_page', IndexPagePreview);
 CMS.registerPreviewTemplate('norwegian_employees', EmployeePreview);
 CMS.registerPreviewTemplate('english_employees', EmployeePreview);
 CMS.registerPreviewTemplate('products', ProductPagePreview);
