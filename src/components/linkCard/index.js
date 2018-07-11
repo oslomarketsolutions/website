211023b41e33bae06d7ad90d3bf2f38824c66514
Åsmund Staldvik <aasmund.staldvik@gmail.com>
2018-07-11 15:21:36 +0200

    Changed to gatsby-img in link cards

--- src/components/linkCard/index.js
+++ src/components/linkCard/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Img from 'gatsby-image';
 import styles from './linkCard.module.scss';
+import { findImageResolution } from '../helperFunctions';
 
-const LinkCard = ({ product, onClickFunction, sticky }) => {
+const LinkCard = ({ product, onClickFunction, sticky, imageResolutions }) => {
   const style = sticky ? styles.sticky : styles.notSticky;
 
   return (
     <button onClick={() => onClickFunction(product.title)} className={style}>
-      <img src={product.image} alt={product.title} />
-      <div>
+      <Img
+        outerWrapperClassName={styles.imageContainer}
+        style={{ height: '100%', width: '100%' }}
+        sizes={findImageResolution(product.image, imageResolutions)}
+      />
+      <div className={styles.textContainer}>
         <h4>{product.title}</h4>
-        <p>{product.description.slice(0, 140)}</p>
       </div>
     </button>
   );
@@ -22,4 +27,5 @@ LinkCard.propTypes = {
   product: PropTypes.arrayOf(PropTypes.object),
   onClickFunction: PropTypes.func,
   sticky: PropTypes.bool,
+  imageResolutions: PropTypes.arrayOf(PropTypes.object),
 };
