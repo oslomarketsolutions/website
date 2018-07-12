606c9bb0462de691a9aa8f673a8d75b5f9eaef58
Åsmund Staldvik <aasmund.staldvik@gmail.com>
2018-07-12 13:07:17 +0200

    Removed gatsby-remark-images as we don't use it

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,17 +53,7 @@ module.exports = {
     {
       resolve: 'gatsby-transformer-remark',
       options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 2560,
-            },
-          },
-        ],
+        plugins: [],
       },
     },
     {
