1e79e292609758fc2f6a806fa8c86f88fce4109e
Elias Thøgersen <elias3004@msn.com>
2018-07-13 13:26:40 +0200

    adding fa-css

--- src/components/previewWrapper/index.js
+++ src/components/previewWrapper/index.js
@@ -1,10 +1,14 @@
+import { dom } from '@fortawesome/fontawesome-svg-core';
 import React from 'react';
 import PropTypes from 'prop-types';
 import typography from '../../utils/typography';
 import '../../layouts/index';
 
+const fontAwesomeCSS = dom.css();
+
 const PreviewWrapper = ({ children }) => (
   <div>
+    <style>{fontAwesomeCSS}</style>
     <style>{typography.toString()}</style>
     {children}
   </div>
