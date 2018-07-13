eca040a52468b1efccadfd4129f216ba68715588
Elias Thøgersen <elias3004@msn.com>
2018-07-13 13:19:25 +0200

    Added PreviewWrapper to all previews

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { BlogPostTemplate } from '../../templates/blogPost/index';
+import PreviewWrapper from '../../components/previewWrapper';
 
 const BlogPostPreview = ({ entry, widgetFor }) => (
-  <BlogPostTemplate
-    content={widgetFor('body')}
-    description={entry.getIn(['data', 'description'])}
-    tags={entry.getIn(['data', 'tags'])}
-    title={entry.getIn(['data', 'title'])}
-  />
+  <PreviewWrapper>
+    <BlogPostTemplate
+      content={widgetFor('body')}
+      description={entry.getIn(['data', 'description'])}
+      tags={entry.getIn(['data', 'tags'])}
+      title={entry.getIn(['data', 'title'])}
+    />
+  </PreviewWrapper>
 );
 
 BlogPostPreview.propTypes = {
