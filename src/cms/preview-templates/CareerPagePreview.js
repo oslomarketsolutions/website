49342881c73b97d990a5e6319ea211b81361ad89
Åsmund Staldvik <aasmund.staldvik@gmail.com>
2018-07-13 08:42:13 +0200

    Career Page now previews correctly in CMS

--- src/cms/preview-templates/CareerPagePreview.js
+++ src/cms/preview-templates/CareerPagePreview.js
@@ -5,14 +5,25 @@ import { CareerPageTemplate } from '../../templates/careerPage/index';
 const CareerPagePreview = ({ entry }) => {
   // I don't think we have access to the perklist from careerPage,
   // it's fetched by a query in CareerPageTemplate
-  const perkList = [];
+  const perkList = [
+    {
+      node: {
+        frontmatter: {
+          title: 'Perk Card',
+          text: 'Disse kortene redigeres på egen side',
+        },
+      },
+    },
+  ];
 
   return (
     <CareerPageTemplate
-      content={entry.getIn(['data', 'body'])}
       title={entry.getIn(['data', 'title'])}
-      text={entry.getIn(['data', 'text'])}
-      image={entry.getIn(['data', 'image'])}
+      image1={entry.getIn(['data', 'image1'])}
+      text1={entry.getIn(['data', 'text1'])}
+      header={entry.getIn(['data', 'header'])}
+      image2={entry.getIn(['data', 'image2'])}
+      text2={entry.getIn(['data', 'text2'])}
       subHeader1={entry.getIn(['data', 'subHeader1'])}
       subHeader2={entry.getIn(['data', 'subHeader2'])}
       perkList={perkList}
