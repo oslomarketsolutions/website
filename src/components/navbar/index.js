60ef7fadc0894ed658f8151f36b93604df6a5134
Elias Thøgersen <elias3004@msn.com>
2018-07-16 16:11:58 +0200

    Changed header position and fixed margin/paddings...
    to compensate. Also added logic so header color changes.

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -6,6 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Helmet from 'react-helmet';
 import Link from 'gatsby-link';
 import logo from '../../img/logo_oms_hoved.png';
+import logoWhite from '../../img/logo_oms_hvit.png';
 import menuGraph from '../../img/menu_graph.png';
 import styles from './navbar.module.scss';
 
@@ -55,116 +56,131 @@ export default class Navbar extends Component {
   };
 
   render() {
+    let navLogoWhite =
+      this.props.location.pathname === '/en' ||
+      this.props.location.pathname === '/no';
+
+    if (this.state.navOpen === true) navLogoWhite = false;
+
     return (
-      <header className={styles.navbar}>
-        <Helmet>
-          <body className={this.state.navOpen ? styles.noScroll : null} />
-        </Helmet>
-        <Link
-          className={`${styles.noHover} ${styles.logo}`}
-          to={`/${this.props.language}`}
-          onClick={this.closeNav}
-        >
-          <img src={logo} alt="Oms logo" />{' '}
-        </Link>
-        <button
-          onClick={this.toggleNav}
-          aria-label={
-            this.props.language === 'en' ? 'Toggle menu' : 'Åpne/lukke meny'
-          }
-        >
-          <div className={styles.hamburger}>
-            <span
-              className={this.state.navOpen ? styles.barOpen : styles.bar}
-            />
-            <span
-              className={this.state.navOpen ? styles.barOpen : styles.bar}
-            />
-            <span
-              className={this.state.navOpen ? styles.barOpen : styles.bar}
-            />
-          </div>
-        </button>
-        <nav className={this.state.navOpen ? styles.open : null}>
-          <ul>
-            <li>
-              <Link
-                activeClassName={styles.active}
-                to={`/${this.props.language}/products`}
-                onClick={this.closeNav}
+      <header
+        className={
+          this.props.location.pathname === '/en' ||
+          this.props.location.pathname === '/no'
+            ? styles.navColorWhite
+            : ''
+        }
+      >
+        <div className={styles.navbar}>
+          <Helmet>
+            <body className={this.state.navOpen ? styles.noScroll : null} />
+          </Helmet>
+          <Link
+            className={`${styles.noHover} ${styles.logo}`}
+            to={`/${this.props.language}`}
+            onClick={this.closeNav}
+          >
+            <img src={navLogoWhite ? logoWhite : logo} alt="Oms logo" />
+          </Link>
+          <button
+            onClick={this.toggleNav}
+            aria-label={
+              this.props.language === 'en' ? 'Toggle menu' : 'Åpne/lukke meny'
+            }
+          >
+            <div className={styles.hamburger}>
+              <span
+                className={this.state.navOpen ? styles.barOpen : styles.bar}
+              />
+              <span
+                className={this.state.navOpen ? styles.barOpen : styles.bar}
+              />
+              <span
+                className={this.state.navOpen ? styles.barOpen : styles.bar}
+              />
+            </div>
+          </button>
+          <nav className={this.state.navOpen ? styles.open : null}>
+            <ul>
+              <li>
+                <Link
+                  activeClassName={styles.active}
+                  to={`/${this.props.language}/products`}
+                  onClick={this.closeNav}
+                >
+                  {this.props.language === 'en' ? 'Products' : 'Produkter'}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClassName={styles.active}
+                  to={`/${this.props.language}/career`}
+                  onClick={this.closeNav}
+                >
+                  {this.props.language === 'en' ? 'Work' : 'Jobb'}
+                </Link>
+              </li>
+              <li className={styles.doublePadding}>
+                <Link
+                  activeClassName={styles.active}
+                  to={`/${this.props.language}/about`}
+                  onClick={this.closeNav}
+                >
+                  {this.props.language === 'en' ? 'About us' : 'Om oss'}
+                </Link>
+              </li>
+              <li
+                className={`${styles.noPaddingRight} ${styles.borderLeft} ${
+                  styles.socialMedia
+                }`}
               >
-                {this.props.language === 'en' ? 'Products' : 'Produkter'}
-              </Link>
-            </li>
-            <li>
-              <Link
-                activeClassName={styles.active}
-                to={`/${this.props.language}/career`}
-                onClick={this.closeNav}
-              >
-                {this.props.language === 'en' ? 'Work' : 'Jobb'}
-              </Link>
-            </li>
-            <li className={styles.doublePadding}>
-              <Link
-                activeClassName={styles.active}
-                to={`/${this.props.language}/about`}
-                onClick={this.closeNav}
-              >
-                {this.props.language === 'en' ? 'About us' : 'Om oss'}
-              </Link>
-            </li>
-            <li
-              className={`${styles.noPaddingRight} ${styles.borderLeft} ${
-                styles.socialMedia
-              }`}
-            >
-              <a
-                href="https://www.linkedin.com/company/oslo-market-solutions-as/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FontAwesomeIcon icon={['fab', 'linkedin']} />
-              </a>
-            </li>
-            <li className={`${styles.noPaddingRight} ${styles.socialMedia}`}>
-              <a
-                href="https://www.facebook.com/oslomarketsolutions/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FontAwesomeIcon icon={['fab', 'facebook']} />
-              </a>
-            </li>
-            <li className={`${styles.noPaddingRight} ${styles.socialMedia}`}>
-              <a
-                href="https://medium.com/shark-bytes"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FontAwesomeIcon icon={['fab', 'medium']} />
-              </a>
-            </li>
-            <li className={`${styles.borderRight} ${styles.socialMedia}`}>
-              <a
-                href="https://github.com/oslomarketsolutions"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FontAwesomeIcon icon={['fab', 'github-square']} />
-              </a>
-            </li>
-            <li className={styles.noPaddingRight}>
-              <Link to={this.changePageLanguage()}>
-                <FontAwesomeIcon icon="globe" />
-                {this.props.language === 'no' ? 'English' : 'Norsk'}
-              </Link>
-            </li>
-          </ul>
-          <div className={styles.menuGraph}>
-            <img src={menuGraph} alt="Graph in the background" />
-          </div>
-        </nav>
+                <a
+                  href="https://www.linkedin.com/company/oslo-market-solutions-as/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={['fab', 'linkedin']} />
+                </a>
+              </li>
+              <li className={`${styles.noPaddingRight} ${styles.socialMedia}`}>
+                <a
+                  href="https://www.facebook.com/oslomarketsolutions/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={['fab', 'facebook']} />
+                </a>
+              </li>
+              <li className={`${styles.noPaddingRight} ${styles.socialMedia}`}>
+                <a
+                  href="https://medium.com/shark-bytes"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={['fab', 'medium']} />
+                </a>
+              </li>
+              <li className={`${styles.borderRight} ${styles.socialMedia}`}>
+                <a
+                  href="https://github.com/oslomarketsolutions"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={['fab', 'github-square']} />
+                </a>
+              </li>
+              <li className={styles.noPaddingRight}>
+                <Link to={this.changePageLanguage()}>
+                  <FontAwesomeIcon icon="globe" />
+                  {this.props.language === 'no' ? 'English' : 'Norsk'}
+                </Link>
+              </li>
+            </ul>
+            <div className={styles.menuGraph}>
+              <img src={menuGraph} alt="Graph in the background" />
+            </div>
+          </nav>
+        </div>
       </header>
     );
   }
