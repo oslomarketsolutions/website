5e7bca7a3ded90e9960485ea38ea4f2ee4cfad4b
Elias Thøgersen <elias3004@msn.com>
2018-07-18 16:42:46 +0200

    Test

--- src/cms/preview-templates/ResponsibleDisclosurePreview.js
+++ src/cms/preview-templates/ResponsibleDisclosurePreview.js
@@ -5,14 +5,7 @@ import PreviewWrapper from '../../components/previewWrapper';
 
 const ResponsibleDisclosurePreview = ({ entry, getAsset }) => (
   <PreviewWrapper>
-    <ResponsibleDisclosureTemplate
-      title={entry.getIn(['data', 'contactInfo', 'title'])}
-      image={getAsset(entry.getIn(['data', 'contactInfo', 'image']))}
-      visitingAddress={entry.getIn(['data', 'contactInfo', 'visitingAddress'])}
-      phoneNumber={entry.getIn(['data', 'contactInfo', 'phoneNumber'])}
-      mailAddress={entry.getIn(['data', 'contactInfo', 'mailAddress'])}
-      content={entry.getIn(['data', 'body'])}
-    />
+    <ResponsibleDisclosureTemplate />
   </PreviewWrapper>
 );
 
