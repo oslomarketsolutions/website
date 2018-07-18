14109501d7e2dc983a7db668a56b8ee5e4927188
Elias Thøgersen <elias3004@msn.com>
2018-07-18 16:53:47 +0200

    test

--- src/cms/preview-templates/ResponsibleDisclosurePreview.js
+++ src/cms/preview-templates/ResponsibleDisclosurePreview.js
@@ -3,9 +3,16 @@ import PropTypes from 'prop-types';
 import { ResponsibleDisclosureTemplate } from '../../templates/responsibleDisclosurePage/index';
 import PreviewWrapper from '../../components/previewWrapper';
 
-const ResponsibleDisclosurePreview = ({ entry, getAsset }) => (
+const ResponsibleDisclosurePreview = ({ entry, getAsset, widgetFor }) => (
   <PreviewWrapper>
-    <ResponsibleDisclosureTemplate />
+    <ResponsibleDisclosureTemplate
+      title={entry.getIn(['data', 'contactInfo', 'title'])}
+      image={getAsset(entry.getIn(['data', 'contactInfo', 'image']))}
+      visitingAddress={entry.getIn(['data', 'contactInfo', 'visitingAddress'])}
+      phoneNumber={entry.getIn(['data', 'contactInfo', 'phoneNumber'])}
+      mailAddress={entry.getIn(['data', 'contactInfo', 'mailAddress'])}
+      content={widgetFor('body')}
+    />
   </PreviewWrapper>
 );
 
@@ -14,6 +21,7 @@ ResponsibleDisclosurePreview.propTypes = {
     getIn: PropTypes.func,
   }),
   getAsset: PropTypes.func,
+  widgetFor: PropTypes.func,
 };
 
 export default ResponsibleDisclosurePreview;
