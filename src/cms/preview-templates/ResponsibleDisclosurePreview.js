8b78763f40c4aa81ec07240e23dae75ea86be7ae
Elias Thøgersen <elias3004@msn.com>
2018-07-18 15:07:26 +0200

    Made page responsive and added CMS preview

---
+++ src/cms/preview-templates/ResponsibleDisclosurePreview.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ResponsibleDisclosureTemplate } from '../../templates/responsibleDisclosurePage/index';
+import PreviewWrapper from '../../components/previewWrapper';
+
+const ResponsibleDisclosurePreview = ({ entry, getAsset }) => (
+  <PreviewWrapper>
+    <ResponsibleDisclosureTemplate
+      title={entry.getIn(['data', 'title'])}
+      image={getAsset(entry.getIn(['data', 'image']))}
+      visitingAddress={entry.getIn(['data', 'visitingAddress'])}
+      phoneNumber={entry.getIn(['data', 'phoneNumber'])}
+      mailAddress={entry.getIn(['data', 'mailAddress'])}
+      content={entry.getIn(['data', 'content'])}
+    />
+  </PreviewWrapper>
+);
+
+ResponsibleDisclosurePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  getAsset: PropTypes.func,
+};
+
+export default ResponsibleDisclosurePreview;
