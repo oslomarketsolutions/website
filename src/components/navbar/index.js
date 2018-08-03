20805ed7212c9fc519df3ac250857d3d3cda61ea
Elias Thøgersen <elias3004@msn.com>
2018-08-03 15:38:58 +0200

    Added correct customer logos and ...
    removed old code.

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import { OutboundLink } from 'gatsby-plugin-google-analytics';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-// import Helmet from 'react-helmet';
 import Link from 'gatsby-link';
 import logo from '../../img/logo_oms_hoved.png';
 import logoWhite from '../../img/logo_oms_hvit.png';
-// import menuGraph from '../../img/menu_graph.png';
 import styles from './navbar.module.scss';
 
 export default class Navbar extends Component {
@@ -97,43 +93,22 @@ export default class Navbar extends Component {
         }`}
       >
         <div className={styles.navbar}>
-          {/* <Helmet>
-            <body className={this.state.navOpen ? styles.noScroll : null} />
-          </Helmet> */}
           <div className={styles.logoWrapper}>
             <Link
               className={`${styles.noHover} ${styles.logo}`}
               to={`/${this.props.language}`}
-              /* onClick={this.closeNav} */
+              onClick={this.closeLanguageSelector}
             >
               <img src={navColorWhite ? logoWhite : logo} alt="Oms logo" />
             </Link>
           </div>
-          {/* <button
-            onClick={this.toggleNav}
-            aria-label={
-              this.props.language === 'en' ? 'Toggle menu' : 'Åpne/lukke meny'
-            }
-          >
-            <div className={styles.hamburger}>
-              <span
-                className={this.state.navOpen ? styles.barOpen : styles.bar}
-              />
-              <span
-                className={this.state.navOpen ? styles.barOpen : styles.bar}
-              />
-              <span
-                className={this.state.navOpen ? styles.barOpen : styles.bar}
-              />
-            </div>
-          </button> */}
           <nav className={this.state.navOpen ? styles.open : null}>
             <ul className="navLinks">
               <li>
                 <Link
                   activeClassName={styles.active}
                   to={`/${this.props.language}/products`}
-                  /* onClick={this.closeNav} */
+                  onClick={this.closeLanguageSelector}
                 >
                   {this.props.language === 'en' ? 'Services' : 'Produkter'}
                 </Link>
@@ -142,7 +117,7 @@ export default class Navbar extends Component {
                 <Link
                   activeClassName={styles.active}
                   to={`/${this.props.language}/career`}
-                  /* onClick={this.closeNav} */
+                  onClick={this.closeLanguageSelector}
                 >
                   {this.props.language === 'en' ? 'Careers' : 'Jobb'}
                   <span>2</span>
@@ -152,51 +127,11 @@ export default class Navbar extends Component {
                 <Link
                   activeClassName={styles.active}
                   to={`/${this.props.language}/about`}
-                  /* onClick={this.closeNav} */
+                  onClick={this.closeLanguageSelector}
                 >
                   {this.props.language === 'en' ? 'About us' : 'Om oss'}
                 </Link>
               </li>
-              {/* <li
-                className={`${styles.noPaddingRight} ${styles.borderLeft} ${
-                  styles.socialMedia
-                }`}
-              >
-                <OutboundLink
-                  href="https://www.linkedin.com/company/oslo-market-solutions-as/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FontAwesomeIcon icon={['fab', 'linkedin']} />
-                </OutboundLink>
-              </li>
-              <li className={`${styles.noPaddingRight} ${styles.socialMedia}`}>
-                <OutboundLink
-                  href="https://www.facebook.com/oslomarketsolutions/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FontAwesomeIcon icon={['fab', 'facebook']} />
-                </OutboundLink>
-              </li>
-              <li className={`${styles.noPaddingRight} ${styles.socialMedia}`}>
-                <OutboundLink
-                  href="https://medium.com/shark-bytes"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FontAwesomeIcon icon={['fab', 'medium']} />
-                </OutboundLink>
-              </li>
-              <li className={`${styles.borderRight} ${styles.socialMedia}`}>
-                <OutboundLink
-                  href="https://github.com/oslomarketsolutions"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FontAwesomeIcon icon={['fab', 'github-square']} />
-                </OutboundLink>
-              </li> */}
               <li
                 className={`${styles.languageSelectorDesktop} ${
                   styles.noPaddingRight
@@ -237,7 +172,6 @@ export default class Navbar extends Component {
                         <FontAwesomeIcon icon="check" />
                       </div>
                       English
-                      {/* {this.props.language === 'no' ? 'English' : 'Norsk'} */}
                     </Link>
                   </li>
                   <li
@@ -262,9 +196,6 @@ export default class Navbar extends Component {
                 </ul>
               </li>
             </ul>
-            {/* <div className={styles.menuGraph}>
-              <img src={menuGraph} alt="Graph in the background" />
-            </div> */}
           </nav>
         </div>
       </header>
--- src/templates/indexPage/index.js
+++ src/templates/indexPage/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { OutboundLink } from 'gatsby-plugin-google-analytics';
-// import Link from 'gatsby-link';
 import styles from './indexPage.module.scss';
 import FeatureCard from '../../components/featureCard';
 import Slider from '../../components/slider/index';
@@ -168,93 +167,6 @@ const IndexPageTemplate = ({ data }) => {
           outerWrapperClassName={styles.imageContainer}
         />
       </section>
-      {/* <section className={styles.animation}>
-        <button className={styles.scrollButton} onClick={onScrollButtonClick}>
-          <FontAwesomeIcon icon={caretDown} size="2x" />
-        </button>
-        <ImageWrapper
-          src={topImage.image}
-          alt={topImage.alt}
-          sizes={findImageSizes(topImage.image, imageSizes)}
-          outerWrapperClassName={styles.imageContainer}
-        />
-      </section>
-      <section className={styles.configurationLogos}>
-        {configurationLogos &&
-          configurationLogos.map(configurationLogo => (
-            <ImageWrapper
-              key={configurationLogo.name}
-              src={configurationLogo.logo}
-              alt={configurationLogo.name}
-              sizes={findImageSizes(configurationLogo.logo, imageSizes)}
-              outerWrapperClassName={styles.imageContainer}
-            />
-          ))}
-      </section>
-      <section className={styles.featuredCase}>
-        <h2>{featuredContent.header}</h2>
-        <p>{featuredContent.text}</p>
-        <ImageWrapper
-          alt={featuredContent.header}
-          src={featuredContent.image}
-          outerWrapperClassName={styles.imageContainer}
-          sizes={findImageSizes(featuredContent.image, imageSizes)}
-        />
-      </section>
-      <section className={styles.customization}>
-        <h2>{customization.header}</h2>
-        <section className={styles.customizationCards}>
-          {customization.cards &&
-            customization.cards.map(customizationCard => (
-              <FeatureCard
-                key={customizationCard.header}
-                title={customizationCard.header}
-                description={customizationCard.description}
-                features={customizationCard.features}
-                sizes={findImageSizes(customizationCard.image, imageSizes)}
-                image={customizationCard.image}
-              />
-            ))}
-        </section>
-      </section>
-
-      <section className={styles.solutions}>
-        <article className={styles.solution}>
-          <ImageWrapper
-            alt={solutionsContent.firstCard.header}
-            src={solutionsContent.firstCard.image}
-            outerWrapperClassName={styles.imageContainer}
-            sizes={findImageSizes(solutionsContent.firstCard.image, imageSizes)}
-          />
-          <h4> {solutionsContent.firstCard.header} </h4>
-          <p> {solutionsContent.firstCard.text} </p>
-        </article>
-        <article className={styles.solution}>
-          <ImageWrapper
-            alt={solutionsContent.secondCard.header}
-            src={solutionsContent.secondCard.image}
-            outerWrapperClassName={styles.imageContainer}
-            sizes={findImageSizes(
-              solutionsContent.secondCard.image,
-              imageSizes,
-            )}
-          />
-          <h4> {solutionsContent.secondCard.header} </h4>
-          <p> {solutionsContent.secondCard.text} </p>
-        </article>
-      </section>
-      <section className={styles.customerLogos}>
-        {customerLogos &&
-          customerLogos.map(customerLogo => (
-            <ImageWrapper
-              key={customerLogo.name}
-              alt={customerLogo.name}
-              src={customerLogo.logo}
-              outerWrapperClassName={styles.imageContainer}
-              sizes={findImageSizes(customerLogo.logo, imageSizes)}
-            />
-          ))}
-      </section> */}
     </main>
   );
 };
