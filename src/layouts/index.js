e7d40758abfee562de41e9285af02283451c649f
Elias Thøgersen <elias3004@msn.com>
2018-08-06 20:11:47 +0200

    Testing some polyfill to fix gatsby-image

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -77,6 +77,8 @@ const TemplateWrapper = ({ children, location, data }) => {
         <link rel="icon" type="image/png" href={favicon} />
         <html lang={language} />
         <style>{fontAwesomeCSS}</style>
+        {/* Below script is a test */}
+        <script src="https://cdn.polyfill.io/v2/polyfill.js?features=default,Symbol" />
         <link
           href="https://fonts.googleapis.com/css?family=Muli:400,600,700,800|Work+Sans:300,400"
           rel="stylesheet"
