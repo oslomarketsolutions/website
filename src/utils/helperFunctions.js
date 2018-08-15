d1272da47d27d070ad98092ce77c3636267e98da
Elias Thøgersen <elias3004@msn.com>
2018-08-15 10:13:22 +0200

    Added checks to see if document is defined

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -26,23 +26,27 @@ export const findImageResolutions = (image, imageResolutions) => {
 };
 
 export const setCookie = (cookieName, cookieValue, expirationDays) => {
-  const d = new Date();
-  d.setTime(d.getTime() + expirationDays * 24 * 60 * 60 * 1000);
-  const expires = `expires=${d.toUTCString()}`;
-  document.cookie = `${cookieName}=${cookieValue};${expires};path=/`;
+  if (document != null) {
+    const d = new Date();
+    d.setTime(d.getTime() + expirationDays * 24 * 60 * 60 * 1000);
+    const expires = `expires=${d.toUTCString()}`;
+    document.cookie = `${cookieName}=${cookieValue};${expires};path=/`;
+  }
 };
 
 export const getCookie = cookieName => {
-  const name = `${cookieName}=`;
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const ca = decodedCookie.split(';');
-  for (let i = 0; i < ca.length; i += 1) {
-    let c = ca[i];
-    while (c.charAt(0) === ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) === 0) {
-      return c.substring(name.length, c.length);
+  if (document != null) {
+    const name = `${cookieName}=`;
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i += 1) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
     }
   }
   return '';
