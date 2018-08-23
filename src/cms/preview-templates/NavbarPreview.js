e550a7f662fb59f08480e98a8b5a466ed95714f2
Elias Thøgersen <elias3004@msn.com>
2018-08-23 21:02:30 +0200

    Fixed navbar preview

--- src/cms/preview-templates/NavbarPreview.js
+++ src/cms/preview-templates/NavbarPreview.js
@@ -9,13 +9,76 @@ const NavbarPreview = ({ entry }) => {
       numberOfJobVacancies: entry.getIn(['data', 'numberOfJobVacancies']),
     },
   };
+
   const location = {
     pathname: '/en',
   };
 
+  const cookiePopUp = {
+    text: 'test',
+    manageButtonText: 'test',
+    confirmationButtonText: 'test',
+  };
+
+  const necessaryCookiesCookies = {
+    name: 'test',
+    purpose: 'test',
+  };
+
+  const necessaryCookies = {
+    id: 'test',
+    header: 'test',
+    text: 'test',
+    cookies: necessaryCookiesCookies,
+  };
+
+  const analyticsCookiesCookies = {
+    name: 'test',
+    purpose: 'test',
+  };
+
+  const analyticsCookies = {
+    id: 'test',
+    header: 'test',
+    text: 'test',
+    cookies: analyticsCookiesCookies,
+  };
+
+  const trackingCookiesCookies = {
+    name: 'test',
+    purpose: 'test',
+  };
+
+  const trackingCookies = {
+    id: 'test',
+    header: 'test',
+    text: 'test',
+    cookies: trackingCookiesCookies,
+  };
+
+  const cookieManager = {
+    necessaryCookies,
+    analyticsCookies,
+    trackingCookies,
+    buttonText: 'test',
+  };
+
+  const cookieInfoEn = {
+    frontmatter: {
+      title: 'test',
+      cookiePopUp,
+      cookieManager,
+    },
+  };
+
   return (
     <PreviewWrapper>
-      <Navbar language="en" location={location} data={data} />
+      <Navbar
+        language="en"
+        location={location}
+        data={data}
+        cookieInfoEn={cookieInfoEn}
+      />
     </PreviewWrapper>
   );
 };
