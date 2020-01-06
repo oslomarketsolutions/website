5ac3f5de8763b6b8e7caf5c2912bbb783716ef2c
elias <elias.thogersen@oms.no>
2020-01-06 13:27:42 +0100

    updated year to 2020

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -187,7 +187,7 @@ const Footer = ({ language, data }) => {
           <div className={styles.copyright}>
             <FontAwesomeIcon icon={['far', 'copyright']} />
             <p className="bodySmall">
-              2018 Oslo Market Solutions. All Rights Reserved.
+              2020 Oslo Market Solutions. All Rights Reserved.
             </p>
           </div>
           <div className={classNames('bodySmall', styles.licenses)}>
